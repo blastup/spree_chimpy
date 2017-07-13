7cdf87a8652904d1d899e1931e6fa633c2b334f1
Dimitris <d.saltaferis@gmail.com>
2017-07-13 14:05:29 +0300

    store default mailchimp lists ids

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -7,13 +7,6 @@ if Spree.user_class
     after_initialize :assign_subscription_default
 
     delegate :subscribe, :resubscribe, :unsubscribe, to: :subscription
-    
-#    after_find :transform_mailchimp_lists_ids
-
-    # def transform_mailchimp_lists_ids
-    #   return if !self.mailchimp_lists_ids
-    #   JSON.parse self.mailchimp_lists_ids
-    # end
 
   private
     def subscription
@@ -21,7 +14,20 @@ if Spree.user_class
     end
 
     def assign_subscription_default
-      self.subscribed ||= Spree::Chimpy::Config.subscribed_by_default if new_record?
+      if new_record? || id_changed?
+        self.subscribed ||= Spree::Chimpy::Config.subscribed_by_default 
+        
+        default_list_id = Spree::Chimpy::Config[:list_id] 
+        if !default_list_id
+          default_list = Spree::Chimpy::Interface::List.new(Spree::Chimpy::Config[:list_name],
+                        Spree::Chimpy::Config.customer_segment_name,
+                        Spree::Chimpy::Config.double_opt_in,
+                        Spree::Chimpy::Config.send_welcome_email,
+                        Spree::Chimpy::Config.list_id)
+          default_list_id = default_list.list_id
+        end
+        self.mailchimp_lists_ids ||= ["#{default_list_id}"].to_json
+      end
     end
   end
 end
